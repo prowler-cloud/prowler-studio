3c7a8c0cf54dbc866c4a421a9cea08ffb58cfbb5
Rubén De la Torre Vico <ruben@prowler.com>
2024-11-11 10:50:17 +0100

    feat: add first custom workflow event the event trigger the creation of metadata

---
+++ ai/src/events.py
@@ -0,0 +1,17 @@
+from llama_index.core.workflow import Event
+from pydantic import Field
+
+
+class CheckMetadataInformation(Event):
+    """Event representing check information for cloud security assessments after user input analysis.
+
+    This event is used to create a new check metadata.
+    """
+
+    check_name: str = Field(description="Name of the check to create")
+    check_description: str = Field(
+        description="Description of the check to create from a cloud cybersecurity perspective"
+    )
+    cloud_provider: str = Field(
+        description="Cloud provider to use for the check creation"
+    )
