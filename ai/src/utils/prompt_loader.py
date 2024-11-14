2c5be2aceb87f9cd75cc25c1910b2566277dd0be
Rubén De la Torre Vico <ruben@prowler.com>
2024-11-14 12:11:45 +0100

    chore: add metadata creation prompt and make new dict to store all different examples for the process

--- ai/src/utils/prompt_loader.py
+++ ai/src/utils/prompt_loader.py
@@ -1,12 +1,16 @@
 from enum import Enum
 
+from ai.src.utils.workflow_check_creation_examples import (
+    EXAMPLE_CHECK_CREATION_WORKFLOW,
+)
+
 
 class Step(str, Enum):
     SECURITY_ANALYSIS = "security_analysis"
     SERVICE_PROVIDER_EXTRACTION = "service_provider_extraction"
-    METADATA_GENERATION = "metadata_generation"
-    TEST_GENERATION = "test_generation"
-    CHECK_CREATION = "check_creation"
+    CHECK_METADATA_GENERATION = "check_metadata_generation"
+    CHKEC_TESTS_GENERATION = "check_tests_generation"
+    CHECK_CODE_GENERATION = "check_code_generation"
 
 
 SYSTEM_CONTEXT_PROMPT = "You are a security engineer specialized in cloud security and python developing working in a cloud security tool called Prowler."
@@ -21,50 +25,78 @@ def load_prompt_template(step: Step, model_reference: str, **kwargs) -> str:
         **kwargs: Additional keyword arguments to be included in the prompt template.
     """
 
+    EXAMPLE_USER_QUERIES = {
+        "aws": ["make a check to ensure that the S3 bucket is not public."],
+        "azure": ["how can I ensure that my Entra policy is secure?"],
+        "gcp": [
+            "create a check to ensure BigQuery datasets are encrypted with Customer-Managed Keys (CMKs)."
+        ],
+        "kubernetes": [
+            "create a check to ensure that in my k8s cluster the secrets are not stored in the environment variables."
+        ],
+    }
+
     RAW_PROMPT_TEMPLATES = {
         "generic": {
-            Step.SECURITY_ANALYSIS: (  # TODO: Improve this template extracting the security requirements, best practices and how to check them step by step. Add more examples for different kind of answers and providers.
+            Step.SECURITY_ANALYSIS: (
                 "Extract the cloud security reasoning and steps behind the user prompt, think step by step how the security comprobation could be done.\n"
-                "User prompt: make a check to ensure that the S3 bucket is not public.\n"
-                "Security analysis: "
-                "The requested prompt is related to cloud security, specifically with AWS because S3 is an AWS for storage. This is a common security best practice to avoid unauthorized access to the bucket content.\nTo do this check you must verify at 4 levels: - Account level: verify that 'Block Public Access' is enabled for the account settings. - Bucket access permissions: verify each bucket has 'Block pulic access through ACLs' and 'Block public access through bucket policies' enabled. - Access control list (ACL): verify that the bucket does not have an ACL with 'AllUsers' or 'Authenticated Users' with 'List' or 'Write' permissions. - Bucket policy: verify that the bucket does not have a bucket policy that allows public access.\n"
-                "User prompt: how can I ensure that my Entra policy is secure?\n"
-                "Security analysis: "
-                "The requested prompt is related to cloud security, specifically with Azure because Entra is the Microsoft product for identity and access management. This is a common security best practice to ensure that the Entra policy is secure to avoid unauthorized access to the resources.\nTo do this check at least you must verify that the Security Defaults are enabled\n"
-                "User prompt: create a check to ensure BigQuery datasets are encrypted with Customer-Managed Keys (CMKs).\n"
-                "Security analysis: "
-                "The requested prompt is related to cloud security, specifically with GCP because BigQuery is a GCP product for data analysis. This is a common security best practice to ensure that the BigQuery datasets are encrypted with Customer-Managed Keys (CMKs) to avoid unauthorized access to the data.\nTo do this check you must verify that the BigQuery datasets are encrypted with Customer-Managed Keys (CMKs).\n"
-                "User prompt: create a check to ensure that in my k8s cluster the secrets are not stored in the enviroment variables.\n"
-                "Security analysis: "
-                "The requested prompt is related to cloud security, specifically with Kubernetes because k8s is the short name for Kubernetes. This is a common security best practice to avoid storing secrets in the environment variables to avoid unauthorized access to the secrets.\nTo do this check you must verify that the secrets are not stored in the environment variables, instead the best practice mounting secrets as files for enhanced security."
+                "In the next lines you can see some examples of the task that you must do. Please, do not copy and paste the examples, you must extract the information from the user prompt.\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES["aws"][0]}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["aws"][0]]['security_analysis']}\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES["azure"][0]}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["azure"][0]]['security_analysis']}\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES["gcp"][0]}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["gcp"][0]]['security_analysis']}\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES["kubernetes"][0]}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["kubernetes"][0]]['security_analysis']}\n"
+                f"{15 * '-'}\n"
+                "Complete only the next task:\n"
                 f"User prompt: {kwargs.get('user_query', '')}\n"
                 "Security analysis: "
             ),
             Step.SERVICE_PROVIDER_EXTRACTION: (
-                "Your task is to extract the service provider and service information from the user prompt.\n"
+                "Extract the service provider and service information from the user prompt.\n"
                 "You MUST return a CheckBasicInformation object, ONLY one string per field.\n"
                 "You can query the security reasoning extracted from other Prowler engineer to help you to extract the service provider and service information. With this information you can design a check name, it should follow the Prowler check naming convention: service_check_description.\n"
                 f"Context: {kwargs.get('security_reasoning', '')}\n"
                 "In the next lines you can see some examples of the task that you must do. Please, do not copy and paste the examples, you must extract the information from the user prompt.\n"
-                "User prompt: make a check to ensure that the S3 bucket is not public.\n"
-                "User prompt analysis:\n"
-                "prowler_provider: aws\n"
-                "service: s3\n"
-                "check_name: s3_bucket_not_public_accesible\n"
-                "User prompt: how can I ensure that my Entra policy is secure?\n"
-                "User prompt analysis:\n"
-                "prowler_provider: azure\n"
-                "service: entra\n"
-                "check_name: entra_security_defaults_enabled\n"
-                "User prompt: create a check to ensure BigQuery datasets are encrypted with Customer-Managed Keys (CMKs).\n"
-                "prowler_provider: gcp\n"
-                "service: bigquery\n"
-                "check_name: bigquery_dataset_cmk_encryption\n"
-                "User prompt: create a check to ensure that in my k8s cluster the secrets are not stored in the enviroment variables.\n"
-                "prowler_provider: kubernetes\n"
-                "service: core\n"
-                "check_name: core_no_secrets_envs\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES['aws'][0]}\n"
+                f"prowler_provider: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['aws'][0]]['prowler_provider']}\n"
+                f"service: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['aws'][0]]['service']}\n"
+                f"check_name: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['aws'][0]]['check_name']}\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES['azure'][0]}\n"
+                f"prowler_provider: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['azure'][0]]['prowler_provider']}\n"
+                f"service: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['azure'][0]]['service']}\n"
+                f"check_name: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['azure'][0]]['check_name']}\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES['gcp'][0]}\n"
+                f"prowler_provider: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['gcp'][0]]['prowler_provider']}\n"
+                f"service: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['gcp'][0]]['service']}\n"
+                f"check_name: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['gcp'][0]]['check_name']}\n"
+                f"User prompt: {EXAMPLE_USER_QUERIES['kubernetes'][0]}\n"
+                f"prowler_provider: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['kubernetes'][0]]['prowler_provider']}\n"
+                f"service: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['kubernetes'][0]]['service']}\n"
+                f"check_name: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES['kubernetes'][0]]['check_name']}\n"
+                f"{15 * '-'}\n"
+                "Complete only the next task:\n"
                 f"User prompt: {kwargs.get('user_query', '')}\n"
+            ),
+            Step.CHECK_METADATA_GENERATION: (
+                "Generate the Prowler check metadata based on a check description.\n"
+                "A Prowler check is a Python script that checks a specific security best practice in a cloud provider service. The metadata of the check is a 'CheckMetadata' object that contains extra the information about the check.\n"
+                "You MUST return a CheckMetadata object.\n"
+                "In the next lines you can see some examples of the task that you must do. Please, do not copy and paste the examples, you must extract the information from the user prompt.\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["aws"][0]]['security_analysis']}\n"
+                f"Check Metadata: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["aws"][0]]['check_metadata']}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["azure"][0]]['security_analysis']}\n"
+                f"Check Metadata: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["azure"][0]]['check_metadata']}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["gcp"][0]]['security_analysis']}\n"
+                f"Check Metadata: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["gcp"][0]]['check_metadata']}\n"
+                f"Security analysis: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["kubernetes"][0]]['security_analysis']}\n"
+                f"Check Metadata: {EXAMPLE_CHECK_CREATION_WORKFLOW[EXAMPLE_USER_QUERIES["kubernetes"][0]]['check_metadata']}\n"
+                f"{15 * '-'}\n"
+                "Complete only the next task:\n"
+                f"Security analysis: {kwargs.get('check_description', '')}\n The CheckID MUST be: {kwargs.get('check_name', '')} and the Provider MUST be: {kwargs.get('prowler_provider', '')}\n"
+                "Check Metadata: "
             ),
         }
     }
