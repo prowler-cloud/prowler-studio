e3f1c123f01791ab6c71e453022f96c83534ff30
Rubén De la Torre Vico <ruben@prowler.com>
2024-11-15 13:19:22 +0100

    feat: add new result events

--- ai/src/events.py
+++ ai/src/events.py
@@ -3,7 +3,10 @@ from typing import Literal
 from llama_index.core.workflow import Event
 from pydantic import Field
 
+from ai.src.utils.llm_structured_outputs import CheckMetadata
 
+
+# TODO: Try to use CheckMetadataInformation and CheckTestInformation as the same event
 class CheckMetadataInformation(Event):
     """Event representing check information for cloud security assessments after user input analysis.
 
@@ -17,3 +20,30 @@ class CheckMetadataInformation(Event):
     prowler_provider: Literal["aws", "azure", "gcp", "kubernetes"] = Field(
         description="Cloud provider to use for the check creation"
     )
+
+
+class CheckTestInformation(Event):
+    """Event representing check information for cloud security assessments after user input analysis.
+
+    This event is used to create a new check metadata.
+    """
+
+    check_name: str = Field(description="Name of the check to create")
+    check_description: str = Field(
+        description="Description of the check to create from a cloud cybersecurity perspective"
+    )
+    prowler_provider: Literal["aws", "azure", "gcp", "kubernetes"] = Field(
+        description="Cloud provider to use for the check creation"
+    )
+
+
+class CheckMetadataResult(Event):
+    """Event representing the output of the check metadata generation step."""
+
+    check_metadata: CheckMetadata = Field(description="Check metadata information")
+
+
+class CheckTestsResult(Event):
+    """Event representing the output of the check metadata generation step."""
+
+    check_tests: str = Field(description="Python tests for the check")
