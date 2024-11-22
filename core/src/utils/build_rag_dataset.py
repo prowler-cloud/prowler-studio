6de301ad1fa8a6ccb0197342bbdf97b3c90f1585
Rubén De la Torre Vico <ruben@prowler.com>
2024-11-22 10:47:50 +0100

    feat: new function to build the index and store it

--- core/src/utils/build_rag_dataset.py
+++ core/src/utils/build_rag_dataset.py
@@ -1,7 +1,11 @@
-from typing import List
+from os.path import abspath, join
+from typing import List, Optional
 
+from llama_index.core import Settings, VectorStoreIndex
 from llama_index.core.schema import Document
 from llama_index.readers.github import GithubClient, GithubRepositoryReader
+
+from core.src.utils.llm_chooser import embedding_model_chooser
 
 
 def extract_prowler_data_from_github(github_token: str) -> List[Document]:
@@ -35,6 +39,30 @@ def extract_prowler_data_from_github(github_token: str) -> List[Document]:
     return reader.load_data(branch="master")
 
 
-def build_rag_dataset(github_token: str):
-    # Extact Documents from Prowler repository
-    extract_prowler_data_from_github(github_token)
+def build_vector_store(
+    github_token: str,
+    model_provider: str,
+    model_reference: str,
+    api_key: Optional[str] = "",
+):
+    """
+    Builds the RAG dataset.
+    This function extracts the data from the Prowler GitHub repository, indexes it, and persists it.
+
+    Args:
+        github_token: The GitHub token used for authentication. Must be fine grained to access the repository.
+        model_provider: Provider of the LLM model.
+        model_reference: Reference to the LLM model, depending on the provider it can be a name, a path or a URL.
+        api_key: API key to access the model. It is not a required parameter if the model provider does not require it.
+    """
+    prowler_documents = extract_prowler_data_from_github(github_token)
+
+    Settings.embed_model = embedding_model_chooser(
+        model_provider=model_provider, model_reference=model_reference, api_key=api_key
+    )
+
+    index = VectorStoreIndex.from_documents(
+        documents=prowler_documents, show_progress=True
+    )
+    core_path = abspath(join(__file__, "../../../"))
+    index.storage_context.persist(join(core_path, "indexed_data_db"))
