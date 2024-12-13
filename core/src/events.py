a48277a672e261cd17e0b2892c1198d812dd2908
Rubén De la Torre Vico <ruben@prowler.com>
2024-12-13 16:44:31 +0100

    refactor(workflow): change events to be totally indepent and only needs minimal information as input

--- core/src/events.py
+++ core/src/events.py
@@ -18,29 +18,36 @@ class CheckBasicInformation(Event):
 
 
 class CheckMetadataInformation(Event):
-    """Event representing check information for cloud security assessments after user input analysis.
-
-    This event is used to create a new check metadata.
-    """
+    """Event representing the information needed to generate check metadata for cloud security assessments after user input analysis."""
 
     check_name: str = Field(description="Name of the check to create")
-    check_description: str = Field(
-        description="Description of the check to create from a cloud cybersecurity perspective"
-    )
+    check_description: str = Field(description="Short description of the check")
     prowler_provider: Literal["aws", "azure", "gcp", "kubernetes"] = Field(
         description="Cloud provider to use for the check creation"
+    )
+    related_check_names: list = Field(
+        description="List of related check names to the check being created"
     )
 
 
 class CheckTestInformation(Event):
-    """Event representing check information for cloud security assessments after user input analysis.
-
-    This event is used to create a new check metadata.
-    """
+    """Event representing information needed to create tests for the check."""
 
     check_name: str = Field(description="Name of the check to create")
-    check_description: str = Field(
-        description="Description of the check to create from a cloud cybersecurity perspective"
+    base_cases_and_steps: str = Field(
+        description="Base cases and steps to identify the security issue"
+    )
+
+
+class CheckCodeInformation(Event):
+    """Event representing check code information needed to create a new check."""
+
+    check_name: str = Field(description="Name of the check to create")
+    base_cases_and_steps: str = Field(
+        description="Base cases and steps to identify the security issue"
+    )
+    related_check_names: list = Field(
+        description="List of related check names to the check being created"
     )
     prowler_provider: Literal["aws", "azure", "gcp", "kubernetes"] = Field(
         description="Cloud provider to use for the check creation"
@@ -57,3 +64,9 @@ class CheckTestsResult(Event):
     """Event representing the output of the check metadata generation step."""
 
     check_tests: str = Field(description="Python tests for the check")
+
+
+class CheckCodeResult(Event):
+    """Event representing the output of the check code generation step."""
+
+    check_code: str = Field(description="Python code for the check")
