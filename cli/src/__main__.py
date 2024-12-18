863d4b079afc161d1612108e2a0631f56255d815
Rubén De la Torre Vico <ruben@prowler.com>
2024-12-18 10:37:12 +0100

    feat(cli): add first cli version

---
+++ cli/src/__main__.py
@@ -0,0 +1,174 @@
+import asyncio
+import os
+import sys
+from typing import Literal
+
+import typer
+from loguru import logger
+from rich.console import Console
+from rich.markdown import Markdown
+from typing_extensions import Annotated
+
+from core.src.utils.build_rag_dataset import build_vector_store
+from core.src.workflow import ChecKreationWorkflow
+
+
+async def run_check_creation_workflow(
+    user_query: str, model_provider: str, model_reference: str, api_key: str
+) -> dict | str:
+    workflow = ChecKreationWorkflow(timeout=60, verbose=False)
+    result = await workflow.run(
+        user_query=user_query,
+        model_provider=model_provider,
+        model_reference=model_reference,
+        api_key=api_key,
+        verbose=False,
+    )
+    return result
+
+
+app = typer.Typer()
+
+console = Console()
+
+
+def set_app_log_level(
+    log_level: Literal[
+        "TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"
+    ],
+) -> None:
+    """Set the minimum log level for the application.
+
+    This function updates the logger to direct log output to a custom log capturing object,
+    filtering logs based on the provided `log_level`. Logs below the specified level will not be shown.
+
+    Args:
+        log_level (Literal["TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"]):
+            The minimum level of logs to display. The available levels are:
+            - "TRACE"
+            - "DEBUG"
+            - "INFO"
+            - "SUCCESS"
+            - "WARNING"
+            - "ERROR"
+            - "CRITICAL"
+        log_capture (LogCapture):
+            An instance of the custom log capture object
+    Raises:
+        ValueError: If the provided log level is invalid.
+    """
+
+    valid_log_levels = {
+        "TRACE",
+        "DEBUG",
+        "INFO",
+        "SUCCESS",
+        "WARNING",
+        "ERROR",
+        "CRITICAL",
+    }
+    if log_level not in valid_log_levels:
+        raise ValueError(
+            f"Invalid log level: {log_level}. Valid options are: {', '.join(valid_log_levels)}"
+        )
+
+    # Remove existing log handlers and set the new log level
+    logger.remove()
+    logger.add(
+        sys.stderr, level=log_level
+    )  # TODO: Use a custom log handler object passed as an argument, https://loguru.readthedocs.io/en/stable/resources/recipes.html#capturing-standard-stdout-stderr-and-warnings. Probably cosole prints should be set in this custom handler class, it should be used as view in MVC pattern
+
+
+@app.command()
+def ask(
+    user_query: str,
+    llm_api_key: Annotated[
+        str, typer.Argument(envvar="LLM_API_KEY")
+    ] = "",  # Is optional because in a future it can support local models
+    embedding_model_api_key: Annotated[
+        str, typer.Argument(envvar="EMBEDDING_MODEL_API_KEY")
+    ] = "",  # TODO: review this because in a future it should support different keys for embedding models and LLM models
+    rag_path: str = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../../", "core", "indexed_data_db"
+    ),
+    log_level: str = "INFO",
+):
+    """Ask a question to Prowler Studio
+
+    Args:
+        user_query (str): User query
+        llm_api_key (Annotated[str, typer.Argument(envvar="GOOGLE_API_KEY")]): Google API key
+    """
+    try:
+        if user_query:
+            if os.path.exists(rag_path):
+                set_app_log_level(log_level)
+
+                result = asyncio.run(
+                    run_check_creation_workflow(
+                        user_query=user_query,
+                        model_provider="gemini",
+                        model_reference="models/gemini-1.5-flash",
+                        api_key=llm_api_key,
+                    )
+                )
+
+                if isinstance(result, dict):
+                    console.print("[bold green]Check Metadata:[/bold green]")
+                    console.print_json(data=result["metadata"])
+                    console.print(
+                        "[bold green]Check Code:[/bold green]\n",
+                        Markdown(result["code"]),
+                    )
+                    console.print(
+                        "[bold green]Check Tests:[/bold green]\n",
+                        Markdown(result["tests"]),
+                    )
+                else:
+                    console.print(f"[italic red]{result}[/italic red]")
+
+            else:
+                raise FileNotFoundError(
+                    "RAG dataset not found, you can build it using `build_check_rag` command"
+                )
+        else:
+            raise ValueError("User query can't be empty")
+    except Exception as e:
+        console.print(f"[bold red]ERROR :cross_mark:: {e}[/bold red]")
+        sys.exit(1)
+
+
+@app.command()
+def build_check_rag(
+    model_provider: str,
+    model_reference: str,
+    github_token: Annotated[str, typer.Argument(envvar="GITHUB_TOKEN")],
+    embedding_model_api_key: Annotated[
+        str, typer.Argument(envvar="EMBEDDING_MODEL_API_KEY")
+    ] = "",
+    rag_path: str = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../../", "core", "indexed_data"
+    ),
+):
+    """Build RAG dataset
+
+    Args:
+        llm_api_key (Annotated[str, typer.Argument(envvar="GOOGLE_API_KEY")]): Google API key
+        llm_api_url (Annotated[str, typer.Argument(envvar="GOOGLE_API_URL")]): Google API URL
+    """
+    try:
+        if os.path.exists(rag_path):
+            raise FileExistsError(f"RAG dataset already exists in the path: {rag_path}")
+        else:
+            console.print("[italic]Building RAG dataset...[/italic]")
+            build_vector_store(
+                github_token=github_token,
+                model_provider=model_provider,
+                model_reference=model_reference,
+                api_key=embedding_model_api_key,
+                # rag_path=rag_path,    TODO: implement this parameter in the function
+            )
+        console.print("[bold green]RAG dataset built successfully![/bold green]")
+    except Exception as e:
+        console.print(f"[bold red]ERROR: {e}[/bold red]")
+        sys.exit(1)
