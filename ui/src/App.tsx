f674e2160b5255d2e5b3eca2ff1a87615c64758a
Rubén De la Torre Vico <ruben@prowler.com>
2025-01-09 12:28:14 +0100

    feat(ui): add base API URL as env variable

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,23 +3,32 @@ import {DeepChat} from 'deep-chat-react';
 import './App.css';
 
 function App() {
+  const baseApiUrl: string = process.env.BASE_API_URL || 'http://localhost:4501';
+
+  console.log('baseApiUrl:', baseApiUrl);
+
   return (
     <div className="App">
       <h1>Prowler Studio</h1>
+      <p>DISCLAIMER: This chat has no memory and does not store any data. It is a simple chat interface to request new checks to Prowler Studio, so don't try to reference previous messages.</p>
       <div className="components">
         <DeepChat
           style={{borderRadius: '10px', width: '96vw', height: 'calc(100vh - 100px)', paddingTop: '10px'}}
           messageStyles={{default: {shared: {innerContainer: {fontSize: '1rem'}}}}}
           inputAreaStyle={{fontSize: '1rem'}}
           introMessage={{text: 'Request any new check to Prowler Studio by typing in the chat box below.'}}
-          connect={{url: 'http://localhost:4501/deployments/ChecKreationWorkflow/tasks/run', method: 'POST', headers: {'Content-Type': 'application/json'}, additionalBodyProps: {user_query: 'Hello!', model_provider: 'gemini', model_reference: 'Flash 1.5', api_key: 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'}}}
-          requestBodyLimits={{maxMessages: 1}}
+          connect={{
+            url: baseApiUrl + '/deployments/ChecKreationWorkflow/tasks/run',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          }}
+          requestBodyLimits={{maxMessages: -1}}
           requestInterceptor={(details: RequestDetails) => {
-            console.log(details);
             return details;
           }}
           responseInterceptor={(response: any) => {
-            console.log(response);
             return response;
           }}
         />
