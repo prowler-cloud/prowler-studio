1c3e6f9ce58743657e8cdec51a32dcb2fe1da4c2
pedrooot <pedromarting3@gmail.com>
2025-01-21 15:43:40 +0100

    fix(ui): revert some changes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
-import { RequestDetails } from 'deep-chat/dist/types/interceptors';
-import { DeepChat } from 'deep-chat-react';
+import {RequestDetails} from 'deep-chat/dist/types/interceptors';
+import {DeepChat} from 'deep-chat-react';
 import './App.css';
 import hljs from "highlight.js";
 import React from 'react';
@@ -25,10 +25,10 @@ function App() {
       <p>DISCLAIMER: This chat has no memory and does not store any data. It is a simple chat interface to request new checks to Prowler Studio, so don't try to reference previous messages.</p>
       <div className="components">
         <DeepChat
-          style={{ borderRadius: '10px', width: '96vw', height: 'calc(80vh - 100px)', paddingTop: '10px' }}
-          messageStyles={{ default: { shared: { innerContainer: { fontSize: '1rem' } } } }}
-          inputAreaStyle={{ fontSize: '1rem' }}
-          introMessage={{ text: 'Request any new check to Prowler Studio by typing in the chat box below.' }}
+          style={{borderRadius: '10px', width: '96vw', height: 'calc(80vh - 100px)', paddingTop: '10px'}}
+          messageStyles={{default:{shared:{innerContainer:{fontSize: '1rem' }}}}}
+          inputAreaStyle={{fontSize: '1rem'}}
+          introMessage={{text: 'Request any new check to Prowler Studio by typing in the chat box below.'}}
           connect={{
             url: baseApiUrl + '/deployments/ChecKreationWorkflow/tasks/run',
             method: 'POST',
