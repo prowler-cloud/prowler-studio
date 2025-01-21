71b80ca847f413eb645144d775714f6e9ff19a6f
pedrooot <pedromarting3@gmail.com>
2025-01-21 15:41:36 +0100

    fix(ui): make smaller chat

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
-import {RequestDetails} from 'deep-chat/dist/types/interceptors';
-import {DeepChat} from 'deep-chat-react';
+import { RequestDetails } from 'deep-chat/dist/types/interceptors';
+import { DeepChat } from 'deep-chat-react';
 import './App.css';
 import hljs from "highlight.js";
 import React from 'react';
@@ -25,10 +25,10 @@ function App() {
       <p>DISCLAIMER: This chat has no memory and does not store any data. It is a simple chat interface to request new checks to Prowler Studio, so don't try to reference previous messages.</p>
       <div className="components">
         <DeepChat
-          style={{borderRadius: '10px', width: '96vw', height: 'calc(100vh - 100px)', paddingTop: '10px'}}
-          messageStyles={{default: {shared: {innerContainer: {fontSize: '1rem'}}}}}
-          inputAreaStyle={{fontSize: '1rem'}}
-          introMessage={{text: 'Request any new check to Prowler Studio by typing in the chat box below.'}}
+          style={{ borderRadius: '10px', width: '96vw', height: 'calc(80vh - 100px)', paddingTop: '10px' }}
+          messageStyles={{ default: { shared: { innerContainer: { fontSize: '1rem' } } } }}
+          inputAreaStyle={{ fontSize: '1rem' }}
+          introMessage={{ text: 'Request any new check to Prowler Studio by typing in the chat box below.' }}
           connect={{
             url: baseApiUrl + '/deployments/ChecKreationWorkflow/tasks/run',
             method: 'POST',
@@ -36,7 +36,7 @@ function App() {
               'Content-Type': 'application/json',
             }
           }}
-          requestBodyLimits={{maxMessages: -1}}
+          requestBodyLimits={{ maxMessages: -1 }}
           requestInterceptor={(details: RequestDetails) => {
             const latestMessage = details.body.messages[details.body.messages.length - 1].text;
             details.body.input = `{"user_query": "${latestMessage}", "model_provider": "gemini", "model_reference": "1.5 Flash"}`;
