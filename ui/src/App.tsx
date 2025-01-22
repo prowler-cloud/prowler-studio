31f4bc50061f84dd4f71f61e450f4c4e4a2ee76f
Pedro Martín <pedromarting3@gmail.com>
2025-01-22 11:34:59 +0100

    fix(ui): make smaller chat (#29)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -25,8 +25,8 @@ function App() {
       <p>DISCLAIMER: This chat has no memory and does not store any data. It is a simple chat interface to request new checks to Prowler Studio, so don't try to reference previous messages.</p>
       <div className="components">
         <DeepChat
-          style={{borderRadius: '10px', width: '96vw', height: 'calc(100vh - 100px)', paddingTop: '10px'}}
-          messageStyles={{default: {shared: {innerContainer: {fontSize: '1rem'}}}}}
+          style={{borderRadius: '10px', width: '96vw', height: 'calc(95vh - 100px)', paddingTop: '10px'}}
+          messageStyles={{default:{shared:{innerContainer:{fontSize: '1rem' }}}}}
           inputAreaStyle={{fontSize: '1rem'}}
           introMessage={{text: 'Request any new check to Prowler Studio by typing in the chat box below.'}}
           connect={{
@@ -36,7 +36,7 @@ function App() {
               'Content-Type': 'application/json',
             }
           }}
-          requestBodyLimits={{maxMessages: -1}}
+          requestBodyLimits={{ maxMessages: -1 }}
           requestInterceptor={(details: RequestDetails) => {
             const latestMessage = details.body.messages[details.body.messages.length - 1].text;
             details.body.input = `{"user_query": "${latestMessage}", "model_provider": "gemini", "model_reference": "1.5 Flash"}`;
