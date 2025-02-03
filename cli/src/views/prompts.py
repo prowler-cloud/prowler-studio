a3f4acd6cff88a1ca49accbf854a84d262571844
Rubén De la Torre Vico <ruben@prowler.com>
2025-02-03 23:53:15 +0100

    chore: add defailt output directory

--- cli/src/views/prompts.py
+++ cli/src/views/prompts.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from rich.prompt import Confirm, Prompt
 
@@ -11,24 +12,24 @@ def prompt_user_message() -> str:
 
 def confirm_save_check() -> bool:
     return Confirm.ask(
-        "\n[bold]Do you want to save this check in your local Prowler repository?[/bold] [y/n]"
+        "\n[bold]Do you want to save the generated check into a directory?[/bold] [y/n]"
     )
 
 
-def ask_prowler_folder() -> str:
+def ask_output_directory() -> Path:
     while True:
         path = Prompt.ask(
-            "\n[bold]Enter the path to your local Prowler repository (or type 'skip' to cancel)[/bold]\n╰┈➤"
+            "\n[bold]Enter the path to save the check or do not write anything to use the default path (./generated_checks/)[/bold]"
         )
-        if path.lower() == "skip":
-            return ""
-        elif os.path.isdir(path):
-            return path
+        if path == "":
+            return Path(os.path.join(os.getcwd(), "generated_checks"))
+        elif os.path.exists(path) and os.path.isdir(path):
+            return Path(path)
         else:
-            display_error("\n[bold red]Invalid path. Please try again.[/bold red]")
+            display_error("\nInvalid output path. Please try again.")
 
 
-def confirm_overwrite(check_path: str) -> bool:
+def confirm_overwrite() -> bool:
     return Confirm.ask(
-        f"\n[bold]The check already exists in: {check_path}. Overwrite?[/bold] [y/n]"
+        "\n[bold]The selected path already contains a check. Do you want to overwrite it?[/bold] [y/n]"
     )
