7d0f96369a7e1fd8898db90dea7d896e9b2f9018
Rubén De la Torre Vico <ruben@prowler.com>
2025-02-04 16:05:43 +0100

    feat: add prowler execution at the end of the check creation

--- cli/src/views/prompts.py
+++ cli/src/views/prompts.py
@@ -5,13 +5,19 @@ def prompt_user_message() -> str:
     return Prompt.ask("\n[bold]Message Prowler Studio :robot:[/bold]\n╰┈➤")
 
 
-def confirm_save_check() -> bool:
+def confirm_save_check(path: str) -> bool:
     return Confirm.ask(
-        "\n[bold]Do you want to save the generated check into a directory?[/bold] [y/n]"
+        f"\n[bold]Do you want to save the generated check in {path}?[/bold] [y/n]"
     )
 
 
 def confirm_overwrite() -> bool:
     return Confirm.ask(
         "\n[bold]The selected path already contains a check. Do you want to overwrite it?[/bold] [y/n]"
+    )
+
+
+def ask_execute_new_check() -> bool:
+    return Confirm.ask(
+        "\n[bold]Do you want to execute a new check?[/bold] (Prowler command must be in the PATH) [y/n]"
     )
