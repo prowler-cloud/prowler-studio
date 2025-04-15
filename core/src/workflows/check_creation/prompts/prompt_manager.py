354c24c1d3cdbe212c7e93964ddae3ca17a15be0
Rubén De la Torre Vico <ruben@prowler.com>
2025-04-15 15:13:01 +0200

    feat: migrate all creation check prompts to jinja2

---
+++ core/src/workflows/check_creation/prompts/prompt_manager.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from jinja2 import (
+    Environment,
+    FileSystemLoader,
+    StrictUndefined,
+    UndefinedError,
+    select_autoescape,
+)
+
+from core.src.workflows.check_creation.utils.prompt_steps_enum import (
+    ChecKreationWorkflowStep,
+)
+from core.src.workflows.utils.prompt_manager import AbstractPromptManager
+
+
+class CheckCreationPromptManager(AbstractPromptManager):
+    def get_prompt(self, step: ChecKreationWorkflowStep, **kwargs) -> str:
+        """Returns the prompt for the given step in the check creation workflow.
+
+        Args:
+            step: The step for which to get the prompt.
+            model_reference: The model reference. Defaults to "generic".
+            **kwargs: Additional arguments to format the prompt.
+        """
+        try:
+            model_template_folder_name = "generic"
+
+            # TODO: Based on the self._model_reference add here an if statement, if there are any specific prompts for specifics mdoels
+            # Example:
+            # if self._model_reference == "gpt-4o-mini":
+            #     model_template_folder_name = "gpt_4o_mini"
+
+            prompt = self._jinja_env.get_template(
+                f"{model_template_folder_name}/{step.value}.jinja"
+            ).render(**kwargs)
+        except FileNotFoundError:
+            raise ValueError(f"Prompt template for step {step.value} not found.")
+        except UndefinedError as e:
+            raise ValueError(
+                f"Undefined variable in prompt template for step {step.value}: {e}"
+            )
+        except Exception as e:
+            raise Exception(f"Error rendering prompt for step {step.value}: {e}")
+
+        return prompt
+
+    def _get_jinja_env(self):
+        """Returns the Jinja2 environment for rendering prompts.
+
+        Returns:
+            The Jinja2 environment.
+        """
+        return Environment(
+            loader=FileSystemLoader(Path(__file__).parent / "templates"),
+            autoescape=select_autoescape(["html", "xml"]),
+            undefined=StrictUndefined,
+        )
