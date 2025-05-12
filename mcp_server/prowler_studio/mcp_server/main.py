e10f484b8844b899e5a71e41195f4b9a0f81d9fc
Rubén De la Torre Vico <ruben@prowler.com>
2025-05-12 10:34:14 +0200

    feat(mcp): add new tool to generate fixers (#72)

--- mcp_server/prowler_studio/mcp_server/main.py
+++ mcp_server/prowler_studio/mcp_server/main.py
@@ -3,9 +3,44 @@ from mcp.server.fastmcp import FastMCP
 
 from prowler_studio.core.workflows.check_creation.events import CheckCreationInput
 from prowler_studio.core.workflows.check_creation.workflow import ChecKreationWorkflow
+from prowler_studio.core.workflows.fixer_creation.events import FixerCreationInput
+from prowler_studio.core.workflows.fixer_creation.workflow import FixerCreationWorkflow
 
 # Initialize FastMCP server
 mcp_server = FastMCP("Prowler Studio MCP Server")
+
+
+@mcp_server.tool("create_prowler_fixer")
+async def create_prowler_fixer(
+    prowler_provider: str, check_id: str, llm_provider: str, llm_reference: str
+) -> str:
+    """Create a Prowler fixer based on a prowler provider and a check ID.
+
+    This process gives all the information needed for a new Prowler fixer creation.
+    If it is not possible to create the fixer, it will return a descriptive error message.
+
+    Args:
+        prowler_provider: Provider of the Prowler for now only "aws" is supported.
+        check_id: ID of the check to which the fixer will be added.
+        llm_provider: Provider of the LLM model, it can be "openai" or "gemini".
+        llm_reference: Reference to the LLM model depending on the provider, it can be "gpt-4o" or "gpt-4o-mini" for OpenAI and "models/gemini-1.5-flash" for Gemini.
+
+    Returns:
+        All the steps to create the fixer with the code and metadata necessary.
+    """
+    try:
+        workflow = FixerCreationWorkflow(timeout=300, verbose=False)
+        result = await workflow.run(
+            start_event=FixerCreationInput(
+                prowler_provider=prowler_provider,
+                check_id=check_id,
+                llm_provider=llm_provider,
+                llm_reference=llm_reference,
+            ),
+        )
+        return result.user_answer
+    except Exception as e:
+        raise e
 
 
 @mcp_server.tool("create_prowler_check")
