4e6c44f51f35abdfe69a2c81ccc15212b1ea3a5a
Rubén De la Torre Vico <ruben@prowler.com>
2025-11-05 08:14:08 +0100

    chore: remove old codebase

--- cli/prowler_studio/_cli/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This module is not meant to be imported directly.
-Use the CLI command 'prowler-studio' instead.
-"""
-
-__all__ = []  # This prevents importing anything from this package
--- core/prowler_studio/core/workflows/fixer_creation/enum_steps.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class FixerCreationWorkflowStep(str, Enum):
-    FIXER_CODE_GENERATION = "fixer_code_generation"
-    PRETIFY_FINAL_ANSWER = "pretify_final_answer"
